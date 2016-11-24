b61d81447a77ac1e597594f28638521c57decbc6
faisal sahak <sahak.faisal@gmail.com>
2016-11-24 22:43:20 +0000

    added some routes, and some pages

--- server.js
+++ server.js
@@ -3,25 +3,46 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 // Middleware //
+app.use(express.static("public"));
 
-
-
+app.set("view engine", "ejs");
 // Routes //
+
+
 app.get("/", (req, res) => {
-  res.send("Hey you!");
+  res.render("");
 });
 
-app.post("/", (req, res) => {
+app.get("/confirm-order", (req, res) => {
+  console.log("confirm-order!");
+}
 
+  res.render('render Confirmation ejs page') {
+
+})
+
+
+app.get('/menus', (req, res)=> {
+  res.render('menu.ejs')
+})
+
+app.get("/current-orders", (req, res) => {
+  res.render("current_orders.ejs" ??);
 });
 
-app.get("/menu", (req, res) => {
 
-});
+app.post("/sendOrder", (req, res) => {
+  client.messages.create({
+        to: twiliosettings.phone,
+        from: twiliosettings.twiliophone,
+        body: "Your order will be ready in 5 minutes!"
+    }, function(err, message) {
+        console.log(message);
+    });
 
-app.put("/menu", (req, res) => {
 
-});
+  res.redirect("")
+}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
